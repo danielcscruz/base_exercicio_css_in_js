53d889e6e12bdb8fa7e0498fbc9846a47fc53340
danielcruz <danielcscruz@gmail.com>
2025-02-10 18:42:52 -0300

    set to styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
+import styled, { createGlobalStyle } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --cor-principal: #a7727d;
+    --cor-secundaria: #f9f5e7;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Lato, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    padding-bottom: 120px;
+  }
+`
+
+const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    max-width: 80%;
+  }
+`
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
